6c131283e29fab027226164be9762d5e69cfd190
bavaria95 <bavaria95@gmail.com>
2015-01-29 22:12:55 +0100

    added reservation of referat topic

--- routes/index.js
+++ routes/index.js
@@ -66,6 +66,7 @@ router.get('/students', function(req, res, next) {
 router.get('/student/:id', function(req, res) {
 	var id = req.params.id;
 	var dict = {};
+	dict['id'] = id;
 	dict['refs'] = [];
 	dict['students_from_group'] = [];
 	dict['notifications'] = [];
@@ -84,6 +85,7 @@ router.get('/student/:id', function(req, res) {
         	dict['group_id'] = row.ID;
             dict['group_day'] = row.Day;
             dict['group_time'] = row.Time;
+            dict['ref_stat'] = row.Referat_status;
         }
     }    
 
@@ -175,121 +177,11 @@ router.get('/grades/:id', function(req, res, next) {
 	db.each("SELECT * FROM Grades WHERE Student = " + id, dbHandler, dbFinal); 
 })
 
-router.get('/test', function(req, res, next) {
-	var d = [
-    {
-        "id": 1,
-        "name": "Item 1",
-        "price": "$1"
-    },
-    {
-        "id": 2,
-        "name": "Item 2",
-        "price": "$2"
-    },
-    {
-        "id": 3,
-        "name": "Item 3",
-        "price": "$3"
-    },
-    {
-        "id": 4,
-        "name": "Item 4",
-        "price": "$4"
-    },
-    {
-        "id": 5,
-        "name": "Item 5",
-        "price": "$5"
-    },
-    {
-        "id": 6,
-        "name": "Item 6",
-        "price": "$6"
-    },
-    {
-        "id": 7,
-        "name": "Item 7",
-        "price": "$7"
-    },
-    {
-        "id": 8,
-        "name": "Item 8",
-        "price": "$8"
-    },
-    {
-        "id": 9,
-        "name": "Item 9",
-        "price": "$9"
-    },
-    {
-        "id": 10,
-        "name": "Item 10",
-        "price": "$10"
-    },
-    {
-        "id": 11,
-        "name": "Item 11",
-        "price": "$11"
-    },
-    {
-        "id": 12,
-        "name": "Item 12",
-        "price": "$12"
-    },
-    {
-        "id": 13,
-        "name": "Item 13",
-        "price": "$13"
-    },
-    {
-        "id": 14,
-        "name": "Item 14",
-        "price": "$14"
-    },
-    {
-        "id": 15,
-        "name": "Item 15",
-        "price": "$15"
-    },
-    {
-        "id": 16,
-        "name": "Item 16",
-        "price": "$16"
-    },
-    {
-        "id": 17,
-        "name": "Item 17",
-        "price": "$17"
-    },
-    {
-        "id": 18,
-        "name": "Item 18",
-        "price": "$18"
-    },
-    {
-        "id": 19,
-        "name": "Item 19",
-        "price": "$19"
-    },
-    {
-        "id": 20,
-        "name": "Item 20",
-        "price": "$20"
-    }
-];
-	/*var d = [{
-        id: 1,
-        name: 'Item 1',
-        price: '$1'
-    }, {
-        id: 2,
-        name: 'Item 2',
-        price: '$2'
-    }];*/
-	res.header("Access-Control-Allow-Origin", "*");
-  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.json(d);
-});
+router.get('/referats/:student/:id', function(req, res, next) {
+	var student = req.params.student;
+	var id = req.params.id;
+	db.run("UPDATE Referats SET Student = ? WHERE ID = ?", [student, id]);
+	res.render('referat', {'id': student});
+})
 
 module.exports = router;
